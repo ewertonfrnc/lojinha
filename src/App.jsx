5db3b848d14d838e002c27ec8b786d4de43667fb
Ewerton Franco <ewerton.silva@usemobile.xyz>
2022-06-23 00:05:44 -0300

    finished the context logic for the cart and the checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ import Navigation from './routes/Navigation/Navigation.component'
 import Home from './routes/Home/Home.component'
 import Shop from './routes/Shop/Shop.component'
 import Authentication from './routes/Authentication/Authentication.component'
+import Checkout from './routes/checkout/checkout.component'
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Navigation />}>
+      <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   )
--- src/components/CartDropdown/CartDropdown.component.jsx
+++ src/components/CartDropdown/CartDropdown.component.jsx
@@ -1,26 +1,31 @@
-import { useContext } from "react";
-import "./cart-dropdown.styles.scss";
+import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 //COMPONENTS
-import Button from "../Button/Button.component";
-import CartItem from "../cart-item/cart-item.component";
+import Button from '../Button/Button.component'
+import CartItem from '../cart-item/cart-item.component'
 
 // CONTEXTS
-import { CartContext } from "../../contexts/cart.context";
+import { CartContext } from '../../contexts/cart.context'
+
+import './cart-dropdown.styles.scss'
 
 const CartDropdown = () => {
-  const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate()
+  const { cartItems } = useContext(CartContext)
+
+  const goToCheckoutHandler = () => navigate('/checkout')
 
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        {cartItems.map((item) => (
+        {cartItems.map(item => (
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>Finalizar Compra</Button>
+      <Button onClick={goToCheckoutHandler}>Finalizar Compra</Button>
     </div>
-  );
-};
+  )
+}
 
-export default CartDropdown;
+export default CartDropdown
