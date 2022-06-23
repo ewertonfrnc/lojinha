52f32a3984fc0d77aadeac21f4a3aafd40f329f8
Ewerton Franco <ewerton.silva@usemobile.xyz>
2022-06-22 22:43:35 -0300

    implemented the styles to the cart dropdown

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const addCartItem = (cartItems, productToAdd) => {
   const existingCartItem = cartItems.find(
@@ -20,14 +20,25 @@ export const CartContext = createContext({
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false),
     [cartItems, setCartItems] = useState([]),
+    [cartCount, setCartCount] = useState(0),
     addItemToCart = (productToAdd) =>
       setCartItems(addCartItem(cartItems, productToAdd)),
-    value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
+    value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount };
+
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
+
+    setCartCount(newCartCount);
+  }, [cartItems]);
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
