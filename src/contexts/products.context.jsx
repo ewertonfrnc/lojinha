6b5b6aa73b3c2e2e109aa5618dcde389b7b36202
Ewerton Franco <ewerton.silva@usemobile.xyz>
2022-06-24 19:46:33 -0300

    implemented function to get the categories from firebase

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect } from 'react'
-import { addCollectionAndDocument } from '../utils/firebase/firebase.utils'
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils'
 
 export const ProductsContext = createContext({
   products: [],
@@ -8,6 +8,15 @@ export const ProductsContext = createContext({
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]),
     value = { products }
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments()
+      console.log(categoryMap)
+    }
+
+    getCategoriesMap()
+  }, [])
 
   return (
     <ProductsContext.Provider value={value}>
