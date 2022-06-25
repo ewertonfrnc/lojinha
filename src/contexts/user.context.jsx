6ddc5d806b45563859804c2a379f2dda3f68a1aa
Ewerton Franco <ewerton.silva@usemobile.xyz>
2022-06-25 13:04:22 -0300

    user context was changed to user reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,19 +1,42 @@
-import { createContext, useState, useEffect } from 'react'
+import { createContext, useState, useEffect, useReducer } from 'react'
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
 } from '../utils/firebase/firebase.utils'
 
-// 1. export the context itself
-// this is the actual value you want to access
 export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => null,
 })
 
-// 2. export the provider
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER',
+}
+
+const userReducer = (state, action) => {
+  const { type, payload } = action
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      }
+
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`)
+  }
+}
+
+const INITIAL_STATE = {
+  currentUser: null,
+}
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null)
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE),
+    setCurrentUser = user =>
+      dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user })
+
   const value = { currentUser, setCurrentUser }
 
   useEffect(() => {
