58813014195b94597f1735c5332e54510d565645
Ewerton Franco <ewerton.silva@usemobile.xyz>
2022-07-02 16:24:07 -0300

    implemented redux thunk and updated the thunk to the categories actions

--- src/store/store.js
+++ src/store/store.js
@@ -2,15 +2,17 @@ import { compose, createStore, applyMiddleware } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 
 import { rootReducer } from './root-reducer'
 
 const persistConfig = { key: 'root', storage, blacklist: ['user'] },
   persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(
-  Boolean
-)
+const middlewares = [
+  process.env.NODE_ENV !== 'production' && logger,
+  thunk,
+].filter(Boolean)
 const composeEnhancers = compose(applyMiddleware(...middlewares))
 
 export const store = createStore(persistedReducer, undefined, composeEnhancers)
