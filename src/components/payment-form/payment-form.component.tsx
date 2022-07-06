f3cb414ecb67dccc6f02597673ccf1749bdfab3e
Ewerton Franco <ewerton.silva@usemobile.xyz>
2022-07-06 11:35:24 -0300

    typed the payment form

--- src/components/payment-form/payment-form.component.tsx
+++ src/components/payment-form/payment-form.component.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, FormEvent, ChangeEvent } from 'react'
 
 import { useSelector } from 'react-redux'
 import { selectCartTotal } from '../../store/cart/cart.selector'
@@ -23,7 +23,7 @@ const PaymentForm = () => {
   const amount = useSelector(selectCartTotal),
     currentUser = useSelector(selectCurrentUser)
 
-  const paymentHandler = async e => {
+  const paymentHandler = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
     if (!stripe || !elements) return
@@ -42,9 +42,13 @@ const PaymentForm = () => {
       paymentIntent: { client_secret },
     } = response
 
+    const cardDetails = elements.getElement(CardElement)
+
+    if (cardDetails === null) return
+
     const paymentResult = await stripe.confirmCardPayment(client_secret, {
       payment_method: {
-        card: elements.getElement(CardElement),
+        card: cardDetails,
         billing_details: {
           name: currentUser ? currentUser.displayName : 'Guest',
         },
