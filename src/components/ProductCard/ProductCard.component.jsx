f2b3ae05a3e08f21c36ab5bd18ba5f9fa0e40581
Ewerton Franco <ewerton.silva@usemobile.xyz>
2023-06-18 00:46:36 -0300

    feat: added currency type

--- src/components/ProductCard/ProductCard.component.jsx
+++ src/components/ProductCard/ProductCard.component.jsx
@@ -18,13 +18,19 @@ const ProductCard = ({ product }) => {
 
   const addProductToCart = () => dispatch(addItemToCart(cartItems, product))
 
+  const formatCurrency = value =>
+    new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(value)
+
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
 
       <div className="footer">
         <span className="name">{name}</span>
-        <span className="price">{price}</span>
+        <span className="price">{formatCurrency(price)}</span>
       </div>
       <Button
         buttonType={BUTTON_TYPE_CLASSES.inverted}
